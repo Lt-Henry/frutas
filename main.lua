c2c9e7f3b6f16a682cd6aafb8167351c688b8dab
Lt-Henry <quiqueiii@gmail.com>
2023-09-18 18:54:30 +0200

    added basic main

---
+++ main.lua
@@ -0,0 +1,27 @@
+function love.load()
+
+end
+
+function love.draw()
+
+end
+
+function love.update(dt)
+
+end
+
+function love.mousepressed(x, y, button, istouch)
+
+end
+
+function love.mousereleased(x, y, button)
+
+end
+
+function love.mousemoved(x, y, dx, dy, istouch )
+
+end
+
+function love.keypressed(key,scancode,isrepeat)
+
+end
